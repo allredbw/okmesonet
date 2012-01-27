0374a6cefdfb04d30af0647b9ce37e14572753c4
Brady Allred <brady.allred@okstate.edu>
2012-01-26 20:54:17 -0600

    add 'R/stations.R'; function to display OK Mesonet stations
    'R/stations.R' downloads and displays OK mesonet station info.

---
+++ R/stations.R
@@ -0,0 +1,32 @@
+stations <- function() {
+  ## Retrieve and display Oklahoma Mesonet station information, including:
+  ## station identifier, station number, station name, nearest town, latitude,
+  ## longitude, elevation, date commissioned, date decommissioned
+  ##
+  ## Arguments: none
+  ## Returns: data frame with above information
+  
+  ## check to see if file has been downloaded by previous function execution
+  ## if so, read file
+  if(file.exists(Sys.glob(file.path(tempdir(),"stationfile*")))==T) {
+    geomeso <- read.csv(Sys.glob(file.path(tempdir(), "stationfile*")), 
+                        skip=122, header=F)
+  } else {
+    ## if file has not been downloaded, download and read file
+    download.file("http://www.mesonet.org/sites/geomeso.csv", 
+                  destfile=tempfile(pattern="stationfile", tmpdir=tempdir()))
+    geomeso <- read.csv(Sys.glob(file.path(tempdir(), "stationfile*")), 
+                        skip=122, header=F)
+  }
+  stationlist <- data.frame(Identifier=geomeso$V2, Number=geomeso$V1,
+                            Name=geomeso$V3, Town=geomeso$V4,
+                            Latitude=geomeso$V8, Longitude=geomeso$V9,
+                            Elevation=geomeso$V10,
+                            Commissioned=strptime(geomeso$V55, format="%Y%m%d",
+                                                  tz="America/Chicago"),
+                            Decommisioned=strptime(ifelse(geomeso$V56<20500101,
+                                                          geomeso$V56, NA), 
+                                                   format="%Y%m%d",
+                                                   tz="America/Chicago")
+                            )
+}
