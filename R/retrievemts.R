8c49b017554262fbd164cab006ebc1c58e0bd8d5
Brady Allred <brady.allred@okstate.edu>
2012-02-02 20:56:49 -0600

    rename hold to mts in retrievemts()

--- R/retrievemts.R
+++ R/retrievemts.R
@@ -7,7 +7,7 @@ retrievemts <- function(station, datemts, getvar) {
   ## Returns: dataframe containing MTS file with timestamp
   
   ## read MTS from Mesonet website
-  hold <- read.csv(paste("http://www.mesonet.org/index.php/dataMdfMts/dataController/getFile/", 
+  mts <- read.csv(paste("http://www.mesonet.org/index.php/dataMdfMts/dataController/getFile/", 
                          format.POSIXct(datemts, format="%Y%m%d"), station, 
                          "/mts/TEXT/", sep = ""), 
                    skip = 2, header = T, as.is = T, sep = "", nrows = 288)
@@ -17,24 +17,20 @@ retrievemts <- function(station, datemts, getvar) {
   ## in MTS file
   ## see http://www.mesonet.org/wiki/Public:MDF_Format
   ## this appears to be always 00:00:00 UTC
-  hold$TIME <- as.POSIXct(hold$TIME*60, origin=datemts)
+  mts$TIME <- as.POSIXct(mts$TIME*60, origin=datemts)
   
-  ## remove MTS TIME variable: can be confusing with timestamp present
-  #hold$TIME <- NULL
-  
-  ## reorder data frame with STID, STNM, and timestamp first
-  
+  ## reorder data frame with STID, STNM, and timestamp first  
   ## variables to put at end
-  endvar <- colnames(hold)[names(hold)!="STID" & names(hold)!="STNM" 
-                           & names(hold)!="TIME"]
+  endvar <- colnames(mts)[names(mts)!="STID" & names(mts)!="STNM" 
+                           & names(mts)!="TIME"]
   
   ## replace data frame with desired column order by subsetting
-  hold <- hold[,c("STID","STNM","TIME",endvar)]
+  mts <- mts[,c("STID","STNM","TIME",endvar)]
   
   ## return data frame with desired vairables
   if(getvar == "ALL") {
-    return(hold)
+    return(mts)
   } else {
-    return(hold[, c("STID", "STNM", "TIME", getvar)])
+    return(mts[, c("STID", "STNM", "TIME", getvar)])
   }
 }
