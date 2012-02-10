234d0fac90ac8d74b8ccabc1851b7dbcf6722da1
Brady Allred <brady.allred@okstate.edu>
2012-02-09 19:28:35 -0600

    add R/avgmts.R; avgmts() function to average MTS over groups
    avgmts() averages an MTS data frame, the output of mts(), by combination
    of staiton, hour, day, month, and/or year

---
+++ R/avgmts.R
@@ -0,0 +1,9 @@
+avgmts <- function(mts, by=c("station")) {
+  ## Averages MTS data frame by station, hour, day, month, or year
+  ## Arguments:
+  ##  mts: MTS data frame provided by mts()
+  ##  by: character, indicating groups to average over; combination of station,
+  ##    hour, day, month, and/or year
+  ## Returns: data frame
+  
+}
