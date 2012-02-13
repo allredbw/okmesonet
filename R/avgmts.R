63b3aa6b13b52bcf32dbaf202b80609cc4c7d4ee
Brady Allred <brady.allred@okstate.edu>
2012-02-13 13:41:15 -0600

    add STNM to avgmts() output

--- R/avgmts.R
+++ R/avgmts.R
@@ -15,21 +15,23 @@ avgmts <- function(mts, groups) {
   }
   
   ## set list for grouping variables
-  group.list <- vector(mode="list", length=length(groups)+1)
+  group.list <- vector(mode="list", length=length(groups)+2)
   
   ## set first grouping to station, identified by mts$STID
   group.list[[1]] <- mts$STID
+  ## set second grouping to station number, identified by mts$STNM
+  group.list[[2]] <- mts$STNM
   
   ## for loop to set grouping variables equal to 'by'
   for(i in 1:length(groups)) {
-    if(groups[i]=="hour") group.list[[i+1]] <- format(mts$TIME, "%H")
-    else if(groups[i]=="day") group.list[[i+1]]  <- format(mts$TIME, "%d")
-    else if(groups[i]=="month") group.list[[i+1]] <- format(mts$TIME, "%m")
-    else if(groups[i]=="year") group.list[[i+1]] <- format(mts$TIME, "%Y")
+    if(groups[i]=="hour") group.list[[i+2]] <- format(mts$TIME, "%H")
+    else if(groups[i]=="day") group.list[[i+2]]  <- format(mts$TIME, "%d")
+    else if(groups[i]=="month") group.list[[i+2]] <- format(mts$TIME, "%m")
+    else if(groups[i]=="year") group.list[[i+2]] <- format(mts$TIME, "%Y")
   }
   
   ## add names to group list
-  names(group.list) <- c("STID",toupper(groups))
+  names(group.list) <- c("STID", "STNM", toupper(groups))
   
   ## variables to average
   avg.var <- colnames(mts)[names(mts)!="STID" & names(mts)!="STNM" 
