c7445147ecb6b9f22ff64ca00b3cc13bcab440eb
Brady Allred <brady.allred@okstate.edu>
2012-03-07 11:22:46 -0600

    add bison data documentation to R/okmesonet.R

--- R/okmesonet.R
+++ R/okmesonet.R
@@ -14,6 +14,13 @@
 #' @aliases okmesonet
 #' @docType package
 NULL
+#' GPS location information for one American bison (\emph{Bison bison}) at The
+#' Nature Conservancy Tallgrass Prairie Preserve, Jan 31 - Feb 5, 2011.
+#' Information includes timestamp, latitude, and longitude.
+#' @docType data
+#' @name bison
+#' @format Data frame.
+NULL
 #' List and attributes of Oklahoma Mesonet stations.
 #'
 #' List of \href{http://www.mesonet.org/}{Oklahoma Mesonet} stations. The
