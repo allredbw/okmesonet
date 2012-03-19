5d893f7c2bd3f311f22f413e142840d4a449e22f
Brady Allred <brady.allred@okstate.edu>
2012-03-19 10:16:53 -0500

    only one grouping variable allowed in avgmts()

--- R/avgmts.R
+++ R/avgmts.R
@@ -1,17 +1,21 @@
-avgmts <- function(mts, groups, metric="mean") {
+avgmts <- function(mts, timeframe, metric="mean") {
   ## Averages MTS data frame by hour, day, month, or year
   ## Arguments:
   ##  mts: MTS data frame provided by mts()
-  ##  by: character, indicating groups to average over:
+  ##  timeframe: character, indicating timeframe to average over:
   ##    hour, day, month, and/or year
   ## Returns: data frame
   
   ## change by to lowercase
-  groups <- tolower(groups)
+  timeframe <- tolower(timeframe)
+  
+  if(length(timeframe)>1) {
+    stop(paste("Only one grouping (timeframe) variable allowed"))
+  }
   
   ## check by for appropriate grouping
-  if(all(groups %in% c("station", "hour", "day", "month", "year"))==FALSE) {
-    stop(c("Grouping variables must be hour, day, month, or year"))
+  if(any(timeframe %in% c("hour", "day", "month", "year"))==FALSE) {
+    stop(c("Grouping (timeframe) variable must be hour, day, month, or year"))
   }
   
   ## check metric for mean, max, or min
@@ -20,30 +24,41 @@ avgmts <- function(mts, groups, metric="mean") {
   }
   
   ## set list for grouping variables
-  group.list <- vector(mode="list", length=length(groups)+2)
+  timeframe.list <- vector(mode="list", length=length(timeframe)+2)
   
   ## set first grouping to station, identified by mts$STID
-  group.list[[1]] <- mts$STID
+  timeframe.list[[1]] <- mts$STID
   ## set second grouping to station number, identified by mts$STNM
-  group.list[[2]] <- mts$STNM
+  timeframe.list[[2]] <- mts$STNM
   
-  ## for loop to set grouping variables equal to 'by'
-  for(i in 1:length(groups)) {
-    if(groups[i]=="hour") group.list[[i+2]] <- format(mts$TIME, "%H")
-    else if(groups[i]=="day") group.list[[i+2]]  <- format(mts$TIME, "%d")
-    else if(groups[i]=="month") group.list[[i+2]] <- format(mts$TIME, "%m")
-    else if(groups[i]=="year") group.list[[i+2]] <- format(mts$TIME, "%Y")
+
+  ## set grouping variables
+  if(timeframe=="hour") {
+    timeframe.list[[3]] <- format(mts$TIME, "%H")
+    timeframe.list[[4]] <- format(mts$TIME, "%d")
+    timeframe.list[[5]] <- format(mts$TIME, "%m")
+    timeframe.list[[6]] <- format(mts$TIME, "%Y")
+    names(timeframe.list) <- c("STID", "STNM", "HOUR", "DAY", "MONTH", "YEAR")
+  } else if (timeframe=="day") {
+      timeframe.list[[3]] <- format(mts$TIME, "%d")
+      timeframe.list[[4]] <- format(mts$TIME, "%m")
+      timeframe.list[[5]] <- format(mts$TIME, "%Y")
+      names(timeframe.list) <- c("STID", "STNM", "DAY", "MONTH", "YEAR")
+  } else if(timeframe=="month") {
+    timeframe.list[[3]] <- format(mts$TIME, "%m")
+    timeframe.list[[4]] <- format(mts$TIME, "%Y")
+    names(timeframe.list) <- c("STID", "STNM","MONTH", "YEAR")
+  } else if(timeframe=="year") {
+    timeframe.list[[3]] <- format(mts$TIME, "%Y")
+    names(timeframe.list) <- c("STID", "STNM", "YEAR")
   }
-  
-  ## add names to group list
-  names(group.list) <- c("STID", "STNM", toupper(groups))
-  
+
   ## variables to average
   ## RAIN is excluded due to its cumulative nature
   avg.var <- colnames(mts)[names(mts)!="STID" & names(mts)!="STNM" 
                           & names(mts)!="TIME" & names(mts)!="RAIN"]
   
   ## calculate averages based on grouping variables
-  mts.avg <- aggregate(mts[,avg.var], by=group.list, FUN=metric, na.rm=T)
+  mts.avg <- aggregate(mts[,avg.var], by=timeframe.list, FUN=metric, na.rm=T)
   return(mts.avg)
 }
