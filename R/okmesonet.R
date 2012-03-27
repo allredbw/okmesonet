24c4c33b5299e3b689f9bc14f7e7b209ecacf337
Brady Allred <brady.allred@okstate.edu>
2012-03-27 16:44:37 -0500

    add checkgeomeso, updatestn, and .onAttach functions to R/okmesonet.R
    .onAttach function is executed when packaged is attached via library() or
    require(); it updates okstations.

--- R/okmesonet.R
+++ R/okmesonet.R
@@ -52,3 +52,52 @@ NULL
 #' @name okstations
 #' @format Data frame
 NULL
+
+checkgeomeso <- function() {
+  ## Check to verify internet connectivity and access to 
+  ## http://www.mesonet.org/sites/geomeso.csv is available.
+  ##
+  ## Arguments: none
+  ## Returns: logical indicating success
+  internetoption <- getOption("internet.info")
+  options(show.error.messages=FALSE, internet.info=3)
+  checkconn <- try(readLines("http://www.mesonet.org/sites/geomeso.csv")[1])
+  options(show.error.messages=TRUE, internet.info=internetoption)
+  if (inherits(checkconn,"try-error")) return(FALSE)
+  else return(TRUE)
+}
+
+updatestn <- function() {
+  ## Retrieve Oklahoma Mesonet station information, including:
+  ## station identifier, station number, station name, nearest town, latitude,
+  ## longitude, elevation, date commissioned, date decommissioned
+  ##
+  ## Arguments: none
+  ## Returns: data frame with above information
+  geomeso <- read.csv("http://www.mesonet.org/sites/geomeso.csv", 
+                      skip=122, header=F, as.is=T)
+  stationlist <- data.frame(Identifier=geomeso$V2, Number=geomeso$V1,
+                            Name=geomeso$V3, Town=geomeso$V4, County=geomeso$V7,
+                            Latitude=geomeso$V8, Longitude=geomeso$V9,
+                            Elevation=geomeso$V10,
+                            Commissioned=strptime(geomeso$V55, format="%Y%m%d",
+                                                  tz="America/Chicago"),
+                            Decommissioned=strptime(geomeso$V56, 
+                                                    format="%Y%m%d",
+                                                    tz="America/Chicago"),
+                            stringsAsFactors=F)
+  return(stationlist)
+}
+
+## onAttach function to update staion list
+.onAttach <- function(libname, pkgname) {
+  if(checkgeomeso()==TRUE) {
+    okstations <- updatestn()
+  } else {
+    warning(paste("Check http://www.mesonet.org/sites/geomeso.csv for", 
+                  " connectivity\n",
+                  "and run data(okstations, package=", dQuote("okmesonet"),")\n",
+                  "to update station list.",
+                  sep=""))
+  }
+}
