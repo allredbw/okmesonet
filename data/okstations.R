a9cdfe5ee1b1d172c1e7e0a575e70441d2f729dc
Brady Allred <brady.allred@okstate.edu>
2012-03-27 22:07:46 -0500

    restructure data/okstations.R
    data/okstations.R restructured for continuity with R/okmesonet.R. Functions are
    prefixed with a '.'

--- data/okstations.R
+++ data/okstations.R
@@ -1,4 +1,4 @@
-checkgeomeso <- function() {
+.checkgeomeso <- function() {
   ## Check to verify internet connectivity and access to 
   ## http://www.mesonet.org/sites/geomeso.csv is available.
   ##
@@ -12,34 +12,34 @@ checkgeomeso <- function() {
   else return(TRUE)
 }
 
-updatestn <- function() {
+.downloadstn <- function() {
   ## Retrieve Oklahoma Mesonet station information, including:
   ## station identifier, station number, station name, nearest town, latitude,
   ## longitude, elevation, date commissioned, date decommissioned
   ##
   ## Arguments: none
   ## Returns: data frame with above information
-  geomeso <- read.csv("http://www.mesonet.org/sites/geomeso.csv", 
-                      skip=122, header=F, as.is=T)
-  stationlist <- data.frame(Identifier=geomeso$V2, Number=geomeso$V1,
-                            Name=geomeso$V3, Town=geomeso$V4, County=geomeso$V7,
-                            Latitude=geomeso$V8, Longitude=geomeso$V9,
-                            Elevation=geomeso$V10,
-                            Commissioned=strptime(geomeso$V55, format="%Y%m%d",
-                                                  tz="America/Chicago"),
-                            Decommissioned=strptime(geomeso$V56, 
-                                                    format="%Y%m%d",
+  
+  ## use .checkgeomeso() to check connectivity
+  if(.checkgeomeso()==FALSE) {
+    warning(paste("Check http://www.mesonet.org/sites/geomeso.csv for", 
+                  " connectivity\n",
+                  "and run updatestn() to update station list.", sep=""))
+  } else {
+    geomeso <- read.csv("http://www.mesonet.org/sites/geomeso.csv", 
+                        skip=122, header=F, as.is=T)
+    stationlist <- data.frame(Identifier=geomeso$V2, Number=geomeso$V1,
+                              Name=geomeso$V3, Town=geomeso$V4, County=geomeso$V7,
+                              Latitude=geomeso$V8, Longitude=geomeso$V9,
+                              Elevation=geomeso$V10,
+                              Commissioned=strptime(geomeso$V55, format="%Y%m%d",
                                                     tz="America/Chicago"),
-                            stringsAsFactors=F)
-  return(stationlist)
+                              Decommissioned=strptime(geomeso$V56, 
+                                                      format="%Y%m%d",
+                                                      tz="America/Chicago"),
+                              stringsAsFactors=F)
+    return(stationlist)
+  }
 }
 
-if(checkgeomeso()==TRUE) {
-  okstations <- updatestn()
-} else {
-  warning(paste("Check http://www.mesonet.org/sites/geomeso.csv for", 
-                " connectivity\n",
-                "and run data(okstations, package=", dQuote("okmesonet"),")\n",
-                "to update station list.",
-            sep=""))
-}
+okstations <- .downloadstn()
