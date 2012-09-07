9e946442a16d99a99e8eb477caa9ccc576a20698
Brady Allred <brady.allred@okstate.edu>
2012-09-07 15:36:22 -0500

    fix UTC bug in totalprecip()
    The rain value at time YYYY-MM-DD 00:00:00 UTC represents the total rainfall
    accumulation for the previous day (UTC). This was originally accounted for, but
    in Oklahoma local time. Corrections now aggregrate by hour, and then sum up to
    day, month, or year as needed.

--- R/totalprecip.R
+++ R/totalprecip.R
@@ -6,55 +6,49 @@ totalprecip <- function(mts, timeframe) {
   ##  timeframe.list: character given by avgmts()
   ## Returns: vector
   
-  ## YYYY-MM-DD 00:00:00 represents the previous day total rainfall
-  ## substract 1 second to get appropriate day
+  ## YYYY-MM-DD 00:00:00 UTC represents the previous day total rainfall
+  ## substract 1 second to get appropriate day/hour
+  
   mts.subset <- mts
-  mts.subset$TIME[which(format(mts.subset$TIME, "%H:%M:%S")=="00:00:00")] <- 
-    mts.subset$TIME[which(format(mts.subset$TIME, "%H:%M:%S")=="00:00:00")] - 1
+  mts.subset$TIME.gmt <- as.POSIXct(format(mts.subset$TIME, tz="GMT"),
+                                    tz="GMT")
+  mts.subset$TIME[which(format(mts.subset$TIME.gmt, 
+                               "%H:%M:%S")=="00:00:00")] <- 
+    mts.subset$TIME[which(format(mts.subset$TIME.gmt, 
+                                 "%H:%M:%S")=="00:00:00")] - 1
   
   ## remove anything outside original timestamps
-  mts.subset <- subset(mts, TIME>=min(mts$TIME) & TIME<=max(mts$TIME))
+  mts.subset <- subset(mts.subset, TIME>=min(mts$TIME) & TIME<=max(mts$TIME))
   
-  if(timeframe=="hour"){
-    ## remove YYYY-MM-DD 00:00:00 values
-    #mts.subset <- subset(mts, format(mts$TIME, "%H:%M:%S")!="00:00:00")
-    
+  ## aggregate by hour
+  
+  ## set list for grouping variables
+  timeframe.list <- vector(mode="list", length=6)
+  ## set first grouping to station, identified by mts$STID
+  timeframe.list[[1]] <- mts.subset$STID
+  ## set second grouping to station number, identified by mts$STNM
+  timeframe.list[[2]] <- mts.subset$STNM
+  
+  ## set grouping variables
+  timeframe.list[[3]] <- format(mts.subset$TIME, "%H")
+  timeframe.list[[4]] <- format(mts.subset$TIME, "%d")
+  timeframe.list[[5]] <- format(mts.subset$TIME, "%m")
+  timeframe.list[[6]] <- format(mts.subset$TIME, "%Y")
+  names(timeframe.list) <- c("STID", "STNM", "HOUR", "DAY", "MONTH", "YEAR")
+  
+  ## calculate rain totals using max-min for each hour
+  rain.total.hour <- aggregate(mts.subset$RAIN, by=timeframe.list, 
+                               FUN=function(RAIN) max(RAIN) - min(RAIN))
+  
+  if(any(timeframe %in% c("day", "month", "year"))) {
     ## set list for grouping variables
-    timeframe.list <- vector(mode="list", length=6)
-    ## set first grouping to station, identified by mts$STID
-    timeframe.list[[1]] <- mts.subset$STID
-    ## set second grouping to station number, identified by mts$STNM
-    timeframe.list[[2]] <- mts.subset$STNM
+    day.list <- list(rain.total.hour$STID, rain.total.hour$STNM,
+                     rain.total.hour$MONTH, rain.total.hour$YEAR,
+                     rain.total.hour$DAY)
+    names(day.list) <- c("STID", "STNM", "MONTH", "YEAR", "DAY")
+    rain.total.day <- aggregate(rain.total.hour$x, by=day.list,
+                                FUN=sum, na.rm=T)
     
-    ## set grouping variables
-    timeframe.list[[3]] <- format(mts.subset$TIME, "%H")
-    timeframe.list[[4]] <- format(mts.subset$TIME, "%d")
-    timeframe.list[[5]] <- format(mts.subset$TIME, "%m")
-    timeframe.list[[6]] <- format(mts.subset$TIME, "%Y")
-    names(timeframe.list) <- c("STID", "STNM", "HOUR", "DAY", "MONTH", "YEAR")
-    
-    ## calculate rain totals using max-min for each hour
-    rain.total.hour <- aggregate(mts.subset$RAIN, by=timeframe.list, 
-                                 FUN=function(RAIN) max(RAIN)-min(RAIN))
-  } else {
-    ## only work with YYYY-MM-DD 00:00:00 values
-    #mts.subset <- subset(mts, format(mts$TIME, "%H:%M:%S")=="00:00:00")
-    
-    ## set list for grouping variables
-    timeframe.list <- vector(mode="list", length=5)
-    ## set first grouping to station, identified by mts$STID
-    timeframe.list[[1]] <- mts.subset$STID
-    ## set second grouping to station number, identified by mts$STNM
-    timeframe.list[[2]] <- mts.subset$STNM
-    
-    ## set grouping variables
-    timeframe.list[[3]] <- format(mts.subset$TIME, "%d")
-    timeframe.list[[4]] <- format(mts.subset$TIME, "%m")
-    timeframe.list[[5]] <- format(mts.subset$TIME, "%Y")
-    names(timeframe.list) <- c("STID", "STNM", "DAY", "MONTH", "YEAR")
-    
-    rain.total.day <- aggregate(mts.subset$RAIN, by=timeframe.list, 
-                                FUN=function(RAIN) max(RAIN))
     if(any(timeframe %in% c("month", "year"))) {
       month.list <- list(rain.total.day$STID, rain.total.day$STNM,
                          rain.total.day$MONTH, rain.total.day$YEAR)
